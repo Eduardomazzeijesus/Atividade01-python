fea7afd81ce8342003b764d87b555001e8d2f62d
Eduardo jesus <jesusameduardo@gmail.com>
2025-03-06 13:38:06 -0300

    Add files via upload

---
+++ atividade-verificaçao-ano.py
@@ -0,0 +1,51 @@
+import os
+os.system("cls")
+
+mes = int(input("digite o seu mês"))
+
+os.system("cls")
+
+print("o mês é " , mes )
+
+if(mes == 1):
+    print("o mês é janeiro")
+
+elif(mes == 2):
+    print("o mês é fevereiro")
+
+elif(mes == 3):
+    print("o mês é março")
+
+elif(mes == 4):
+    print("o mês é abril")
+
+elif(mes == 5):
+    print("o mês é maio")
+
+elif(mes == 6):
+    print("o mês é junho")
+
+elif(mes == 7):
+    print("o mês é julho")
+
+elif(mes == 8):
+    print("o mês é agosto")
+
+elif(mes == 9):
+    print("o mês é setembro")
+
+elif(mes == 10):
+    print("o mês é outubro")
+
+elif(mes == 11):
+    print("o mês é novembro")
+
+elif(mes == 12):
+    print("o mês é dezembro")
+
+else:
+    print("o mês esta errado")
+
+
+
+
